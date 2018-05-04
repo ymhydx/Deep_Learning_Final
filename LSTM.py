dc095406bd62a61c1e63276b0453abcf3556bda2
ymhydx <ymhydx@gmail.com>
2018-05-04 10:43:01 +0200

    LSTM finished.

--- LSTM.py
+++ LSTM.py
@@ -11,7 +11,7 @@ class LSTM:
     def build(self, input_vgg: tf.placeholder, labels: tf.placeholder, n_hidden, init_type, learning_rate, opt_type,
               dropout=False, keep_prob=1):
         num_class = 51
-        # input_vgg = tf.placeholder(tf.float32, shape=[n_frames, 1000, 1])  # input from vgg19
+        # input_vgg = tf.placeholder(tf.float32, shape=[None, num_frames, 1000])  # input from vgg19
         cell = tf.contrib.rnn.LSTMCell(n_hidden)
         if dropout:
             cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=keep_prob)
@@ -27,9 +27,16 @@ class LSTM:
         output = tf.matmul(output_last, W) + b
 
         pred = tf.nn.softmax(output)
-        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=labels))
+        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=pred, labels=labels))
         if opt_type == 'adam':
             optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss=cross_entropy)
         elif opt_type == 'sgd':
             optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss=cross_entropy)
         return output, optimizer
+
+
+input_vgg = tf.placeholder(tf.float32, shape=[100, 50, 1000])
+labels = tf.placeholder(tf.float32, shape=[100, 51])
+lstm = LSTM(input_vgg, labels, 16, 'normal', 0.1, 'adam')
+output, optimizer = lstm.output, lstm.optimizer
+print(output.shape)
