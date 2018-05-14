f452fb58a2f1db0468f778c242245a1331b0c795
ymhydx <ymhydx@gmail.com>
2018-05-14 13:12:50 +0200

    Finish concatenation.

--- Concatenate.py
+++ Concatenate.py
@@ -2,10 +2,22 @@ import Package
 import os
 import numpy as np
 
+dataset = []
+labels = []
 one_hot = Package.one_hot_encoder(51)
 folders = os.listdir('./after_vgg19')
+
+cls = 0
 for folder in folders:
     files = os.listdir('./after_vgg19' + '/' + folder)
     for file in files:
         features = np.load('./after_vgg19' + '/' + folder + '/' + file)
-        
+        dataset.append(features.tolist())
+        labels.append(one_hot[cls])
+    cls += 1
+
+dataset = np.array(dataset)
+labels = np.array(labels)
+
+print(dataset.shape)
+print(labels.shape)
